bcdcb6473c00a2c6e488027dfbd9276252291037
Carlos Lorenzo <carloren.1996@gmail.com>
2025-06-20 11:49:06 +0000

    funciones de BD

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,21 +1,67 @@
-import { React, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [taskList, setTaskList] = useState(["Limpiar", "Tender", "Cocinar"])
+	const [taskList, setTaskList] = useState([])
+
+	function getTaskList() {
+		fetch('https://playground.4geeks.com/todo/users/carloren', { method: "GET" }) //busca info en la url
+			.then((response) => response.json()) 			// promete que si llega la info la guardo en un formato, en este caso json (no es una extensión, es formato de datos) para poder interpretarla y crear una clase
+			.then((data) => setTaskList(data.todos))  	//promete que si se formatea bien lo guarda en un espacio, en este caso data, y lo procesa, en ese caso, hace un console.log
+			.catch((error) => console.log(error)) 			//si algo sale mal, avisa
+	}
 
 	function addTask(event) {
 		if (event.key === "Enter") {
 
-			setTaskList(taskList.concat(event.target.value))
+			fetch('https://playground.4geeks.com/todo/todos/carloren', {
+				method: "POST",
+				body: JSON.stringify({
+					"label": event.target.value,
+					"is_done": false
+				}),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			})
+				.then(resp => {
+					console.log(resp.ok); // Será true si la respuesta es exitosa
+					console.log(resp.status); // El código de estado 201, 300, 400, etc.
+					return resp.json(); // Intentará parsear el resultado a JSON y retornará una promesa donde puedes usar .then para seguir con la lógica
+				})
+				.then(data => {
+					// Aquí es donde debe comenzar tu código después de que finalice la búsqueda
+					getTaskList()
+
+					console.log(data); // Esto imprimirá en la consola el objeto exacto recibido del servidor
+				})
+				.catch(error => {
+					// Manejo de errores
+					console.log(error);
+				});
+
 
 			event.target.value = ""
+
+
 		}
 
 	}
+
+	function deleteTask(id) {
+		fetch('https://playground.4geeks.com/todo/todos/' + id, { method: "DELETE" }).then().then(() => getTaskList()).catch(error => {
+			// Manejo de errores
+			console.log(error);
+		});
+
+	}
+	useEffect(() => {
+		//código que queremos que se ejecute al cargar el componente
+		getTaskList()
+	}, [])
 
 	return (
 		<div id="taskList" className="d-flex flex-column text-center">
@@ -23,11 +69,11 @@ const Home = () => {
 			<ul id="todo" className="shadow-lg border list-group border align-self-center m-0">
 				<li className="border-0 border-bottom list-group-item align-self-center fs-5" style={{ width: "20em" }}><input type="text" className="w-100 px-0" placeholder="Añadir tarea" onKeyDown={addTask} /></li>
 
-				{taskList.map((item, i) => (
-					<li key={i} className="d-flex border-0 border-bottom list-group-item align-self-center row fs-5" style={{ width: "20em" }}>
-						<p className="col-11 align-self-start m-0 px-0 text-start">{item}</p>
+				{taskList.map((item) => (
+					<li key={item.id} className="d-flex border-0 border-bottom list-group-item align-self-center row fs-5" style={{ width: "20em" }}>
+						<p className="col-11 align-self-start m-0 px-0 text-start">{item.label}</p>
 						<p className="col-1 align-self-end text-end m-0 hide">
-							<button className="crossButton" onClick={() => setTaskList(taskList.filter((_, index) => index !== i))}>×</button>
+							<button className="crossButton" onClick={() => deleteTask(item.id)}>×</button>
 						</p>
 					</li>
 				))}
